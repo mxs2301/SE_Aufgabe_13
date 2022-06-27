03cafc559fe50eccc1a4d324cbf79ce38cf2a6e6
Max Schopf <max.schopf@mailfence.com>
2022-06-27 12:50:37 +0200

    Restart

--- src/main/java/com/schopf/Main.java
+++ src/main/java/com/schopf/Main.java
@@ -2,7 +2,6 @@ package com.schopf;
 
 public class Main{
 	public static void main(String[] args) {
-		System.out.println("Black and Blue");
-		System.out.println("Kill me brother");
+		System.out.println("HI");
 	}
 }
